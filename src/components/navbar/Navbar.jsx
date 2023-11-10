f992df7e7abcea6764af4fc7d172f4cfc2446fa2
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2023-11-11 00:48:57 +0530

    Complete hero section

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,14 +5,15 @@ import { motion } from "framer-motion";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <Sidebar />
+      {/* Sidebar */}
+      <Sidebar/>
       <div className="wrapper">
         <motion.span
           initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
         >
-          Suwantha
+          Suwantha 
         </motion.span>
         <div className="social">
           <a href="#">
@@ -24,6 +25,7 @@ const Navbar = () => {
           <a href="#">
             <img src="/twitter.png" alt="" />
           </a>
+          
         </div>
       </div>
     </div>
