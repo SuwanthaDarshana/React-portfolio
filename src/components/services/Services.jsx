3d725a7c77e2ae308cebc580e7dd89e1b63179ee
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2023-11-13 14:03:58 +0530

    update

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -43,7 +43,7 @@ const Services = () => {
       </motion.div>
       <motion.div className="titleContainer" variants={variants}>
         <div className="title">
-          <img src="./people.webp" alt="" />
+          <img src="./people.webp" alt="people" />
           <h1>
             <motion.b whileHover={{ color: "orange" }}>Unique</motion.b> Ideas
           </h1>
