0cd256534f43880d95c9d4a1c8755a0e890f6d98
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2023-11-13 11:45:42 +0530

    update the assets folder

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,19 +6,19 @@ const items = [
   {
     id: 1,
     title: "Student Management System", 
-    img: "../../../public/edulabs.png",
+    img: "./edulabs.png",
     desc:"Experience efficiency with our Student Management System. Built on Angular and Spring Boot, it ensures seamless data management from enrollment to grading. Elevate your institution with this modern and user-friendly solution.",
   },
   {
     id: 2,
     title: "Secret Message Web App",
-    img: "../../../public/secret.png",
+    img: "./secret.png",
     desc: "It's a secret message display site built with Node.js, MongoDB, and secured with Passport.js. Users can log in with their email and password or simply use their Google account for easy access. You can share your secrets or read messages from other users, making it a fun and interactive space for everyone.",
   },
   {
     id: 3,
     title: "Gaming Web UI",
-    img: "../../../public/gameweb.png",
+    img: "./gameweb.png",
     desc:"Dive into gaming excellence with our sleek and immersive web interface. Trending titles, seamless navigation, and stunning visuals await, enhancing your gaming journey. Elevate your experience with an interface designed for both style and functionality."
   },
 ];
